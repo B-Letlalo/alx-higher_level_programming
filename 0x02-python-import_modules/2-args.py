96856323d2a1ca0c133610f19640a4fbf867ae61
B-Letlalo <letlaloboikano@gmail.com>
2023-12-03 06:27:45 -0800

    Counting and printing Command Line Arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+i = 1
+counter = 1
+
+num_args = len(sys.argv)
+if (num_args == 1):
+    print(f"0 arguments")
+else:
+    # Number of arguments here excludes the script name
+    print(f"{num_args - 1} arguments:")
+    for i in range(1, num_args):
+        print(f"{i}: {sys.argv[i]}")
