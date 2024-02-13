44673eaadd7bbfc1321f3f3741cfe664e51238c8
B-Letlalo <letlaloboikano@gmail.com>
2024-02-13 09:01:43 -0800

    Correction: 4-hidden.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
 
-def_names = dir(hidden_4)
-print(f"{def_names}")
+if (__name__ == "__main__"):
+    import hidden_4
+
+    def_names = dir(hidden_4)
+    for def_names in def_names:
+        if def_names[:2] != "__":
+            print(def_names)
