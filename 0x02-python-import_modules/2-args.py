634f2038cc4cd28e4a596840002a605668ce1c1f
B-Letlalo <letlaloboikano@gmail.com>
2024-02-13 08:22:09 -0800

    Correction: 1-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,9 @@ num_args = len(sys.argv)
 if (__name__ == "__main__"):
     if (num_args == 1):
         print(f"0 arguments")
+    elif (num_args == 2):
+        print(f"1 argument:")
+        print(f"1: {sys.argv[i]}")
     else:
         # Number of arguments here excludes the script name
         print(f"{num_args - 1} arguments:")
