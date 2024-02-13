602862a4922c5c68fd19a9a0a4b47fa381735f4e
B-Letlalo <letlaloboikano@gmail.com>
2024-02-13 08:14:21 -0800

    Correction: 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,10 +6,10 @@ b = 5
 
 if (__name__ == "__main__"):
     addition = add(a, b)
-    print(f"{a} + {b} = {addition}")
+    print("{} + {} = {}".format(a, b, addition))
     subtraction = sub(a, b)
-    print(f"{a} - {b} = {subtraction}")
+    print("{} - {} = {}".format(a, b, subtraction))
     multiplication = mul(a, b)
-    print(f"{a} * {b} = {multiplication}")
+    print("{} * {} = {}".format(a, b, multiplication))
     division = div(a, b)
-    print(f"{a} / {b} = {division}")
+    print("{} / {} = {}".format(a, b, division))
